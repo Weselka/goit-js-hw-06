12f6cb221e1d1a095b07f593321b976d3d2bdea1
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-09-12 23:37:46 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('ul');
+
+const makeItem = options => {
+  return options.map(option => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = option;
+    return itemEl;
+  });
+};
+
+const elements = makeItem(ingredients);
+
+list.append(...elements);
