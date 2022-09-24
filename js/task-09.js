38c82c75b8e6d10e482876f7f9a79ab00caf4be5
Weselka <weselyna.shvets@gmail.com>
2022-09-24 18:37:59 +0300

    task-09 update

--- js/task-09.js
+++ js/task-09.js
@@ -8,6 +8,20 @@
 //     .padStart(6, 0)}`;
 // }
 
+const refs = {
+  body: document.querySelector('body'),
+  spanValue: document.querySelector('.color'),
+  btnChangeColor: document.querySelector('.change-color'),
+};
+
+refs.btnChangeColor.addEventListener('click', onClickChangeColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
+}
+
+function onClickChangeColor(event) {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.spanValue.replaceChildren(getRandomHexColor(toString));
 }
