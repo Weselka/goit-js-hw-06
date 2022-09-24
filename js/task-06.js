db3601aff4344dc258218719d88ca32cf70d3dec
Weselka <weselyna.shvets@gmail.com>
2022-09-24 11:49:33 +0300

    task-6 update

--- js/task-06.js
+++ js/task-06.js
@@ -5,12 +5,34 @@ input.addEventListener('blur', onInputBlur);
 function onInputBlur(event) {
   const inputValue = event.currentTarget.value.trim();
   const inputAtribute = Number(input.getAttribute('data-length'));
-
+  replaceClassList('invalid', 'valid');
   if (inputAtribute === inputValue.length) {
-    input.removeAttribute('class');
-    input.classList.add('valid');
-  } else {
-    input.removeAttribute('class');
-    input.classList.add('invalid');    
+    replaceClassList('valid', 'invalid');
+  }
+  if (inputValue.length === 0) {
+    removeAllList('valid', 'invalid');
   }
 }
+
+const replaceClassList = (add, remove) => {
+  input.classList.remove(remove);
+  input.classList.add(add);
+};
+
+const removeAllList = (one, two) => {
+  input.classList.remove(one);
+  input.classList.remove(two);
+};
+
+// function onInputBlur(event) {
+//   const inputValue = event.currentTarget.value.trim();
+//   const inputAtribute = Number(input.getAttribute('data-length'));
+
+//   if (inputAtribute === inputValue.length) {
+//     input.removeAttribute('class');
+//     input.classList.add('valid');
+//   } else {
+//     input.removeAttribute('class');
+//     input.classList.add('invalid');
+//   }
+// }
